0aa1fa979a04c733accda3b6c76b4116ec6aae38
Michael Tambornino <michael.tambornino@gmail.com>
2017-07-06 02:04:31 -0500

    buton listener

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,12 @@
-console.log(`this seems to be working`)
+console.log('this seems to be working')
 
-console.log(`this does too`)
+const submitBtn = document.querySelector('.submit-btn')
+submitBtn.addEventListener('click', () => {
+  console.log('submit button was clicked')
+})
+
+$( document ).ready( function () {
+
+  console.log('Just in case we want some jQuery')
+
+});
