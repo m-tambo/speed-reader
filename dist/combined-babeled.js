ad22ffb3d0f56e2eea5997766ef31e7d73889b8e
Michael Tambornino <michael.tambornino@gmail.com>
2017-07-06 16:23:19 -0500

    adding event listeners and reveal function

--- dist/combined-babeled.js
+++ dist/combined-babeled.js
@@ -2,13 +2,33 @@
 
 console.log('this seems to be working');
 
-var submitBtn = document.querySelector('.submit-btn');
-submitBtn.addEventListener('click', function () {
-  console.log('submit button was clicked');
+// _______targets_______
+var submitForm = document.querySelector('.search-div');
+var resultsDiv = document.querySelector('.results-div');
+var nameOfURL = document.querySelector('.url-input');
+var resultsDivHeader = document.querySelector('.url-name');
+
+// ______functions_______
+var revealResults = function revealResults() {
+  console.log('revealing the results for ' + nameOfURL.value);
+  resultsDivHeader.innerHTML = nameOfURL.value;
+  resultsDiv.removeAttribute('hidden');
+};
+
+var clearInputField = function clearInputField() {
+  nameOfURL.value = '';
+};
+
+// _______events_______
+submitForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  revealResults();
 });
 
-$(document).ready(function () {
-
-  console.log('Just in case we want some jQuery');
+nameOfURL.addEventListener('click', function () {
+  clearInputField();
 });
+
+// _______in case we want some jQuery_______
+$(document).ready(function () {});
 //# sourceMappingURL=combined-babeled.js.map
--- dist/combined.js
+++ dist/combined.js
@@ -1,12 +1,35 @@
-console.log('this seems to be working')
+console.log(`this seems to be working`);
 
-const submitBtn = document.querySelector('.submit-btn')
-submitBtn.addEventListener('click', () => {
-  console.log('submit button was clicked')
+// _______targets_______
+const submitForm = document.querySelector('.search-div');
+const resultsDiv = document.querySelector('.results-div');
+let nameOfURL = document.querySelector('.url-input');
+let resultsDivHeader = document.querySelector('.url-name');
+
+// ______functions_______
+const revealResults = () => {
+  console.log(`showing the results for ${nameOfURL.value}`);
+  resultsDivHeader.innerHTML = nameOfURL.value
+  resultsDiv.removeAttribute('hidden');
+}
+
+const clearInputField = () => {
+  nameOfURL.value = ''
+}
+
+
+
+// _______events_______
+submitForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  revealResults();
+});
+
+nameOfURL.addEventListener('click', () => {
+  clearInputField()
 })
 
-$( document ).ready( function () {
-
-  console.log('Just in case we want some jQuery')
+// _______in case we want some jQuery_______
+$( document ).ready( () => {
 
 });
